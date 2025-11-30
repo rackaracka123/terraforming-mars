43218bc05d80ea90f81456141c3367d461989a31
Mats-Hjalmar <matte.hj1234@gmail.com>
2025-11-30 22:54:06 +0100

    Rawr

--- frontend/src/components/ui/overlay/WaitingRoomOverlay.tsx
+++ frontend/src/components/ui/overlay/WaitingRoomOverlay.tsx
@@ -17,14 +17,6 @@ const WaitingRoomOverlay: React.FC<WaitingRoomOverlayProps> = ({
   const isHost = game.hostPlayerId === playerId;
   const joinUrl = `${window.location.origin}/join?code=${game.id}`;
 
-  // Debug logging
-  console.log('WaitingRoom Debug:', {
-    gameId: game.id,
-    hostPlayerId: game.hostPlayerId,
-    currentPlayerId: playerId,
-    isHost,
-    gameStatus: game.status
-  });
 
   const handleStartGame = () => {
     // Start Game button clicked, ishost: isHost
